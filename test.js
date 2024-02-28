c356b5cf487cc676345d8a442403967dcba0ddd0
Jonathan Gil <main@Jonathans-MacBook-Air.local>
2024-02-28 11:14:41 -0500

    initial commit

---
+++ test.js
@@ -0,0 +1,11 @@
+function myfunction(){
+	document.getElementById("demo").innerHTML = "🤩Supercalifragilisticexpialidocious!!!🤩";
+}
+
+function popup(){
+	alert("😭😭HELP!!😭😭");
+}
+
+function pix(){
+	document.getElementById("picture").src = "button.png";
+}
