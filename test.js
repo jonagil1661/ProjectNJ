f1c98c1e8644577408872c32ee26ed99c58ce611
jonagil1661 <jona.gil1661@gmail.com>
2024-02-28 19:28:11 -0500

    added input and math into this test

--- test.js
+++ test.js
@@ -7,5 +7,31 @@ function popup(){
 }
 
 function pix(){
-	document.getElementById("picture").src = "button.png";
+	document.getElementById("picture").src = "images/button.png";
+}
+
+
+
+
+
+function addValues(){
+	//Create a variable to Return the value of the input from input box id = firstNumber
+	var num1 = document.getElementById("firstNumber").value;
+	//Create a variable to Return the value of the input from input box id = secondNumber
+	var num2 = document.getElementById("secondNumber").value;
+	//create an equation that adds the two inputs together
+	var sum = parseInt(num1) + parseInt(num2) ;
+	//outputs the variable,sum, into the value of input box id = resultNumber
+	document.getElementById("resultNumber").value = sum + " 😎";
+}
+
+function minusValues(){
+	//Create a variable to Return the value of the input from input box id = firstNum
+	var num1 = document.getElementById("firstNum").value;
+	//Create a variable to Return the value of the input from input box id = secondNum
+	var num2 = document.getElementById("secondNum").value;
+	//create an equation that subtracts the inputs
+	var sum = parseInt(num1) - parseInt(num2);
+	//outputs the variable,sum, into the value of input box id = resultNum
+	document.getElementById("resultNum").value = sum + " 🥳";
 }
